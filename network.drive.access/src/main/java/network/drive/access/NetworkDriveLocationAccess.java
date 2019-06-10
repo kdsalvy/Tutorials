2d0e7ce8f3a4aaf64fbf5b51c0fafcda6ee65b70
kdsalvy <saukedia1@publicisgroupe.net>
2019-06-10 16:22:54 +0530

    Fixed read file issue

--- network.drive.access/src/main/java/network/drive/access/NetworkDriveLocationAccess.java
+++ network.drive.access/src/main/java/network/drive/access/NetworkDriveLocationAccess.java
@@ -1,15 +1,12 @@
 package network.drive.access;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.hierynomus.msdtyp.AccessMask;
 import com.hierynomus.msfscc.fileinformation.FileAllInformation;
@@ -17,9 +14,9 @@ import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation;
 import com.hierynomus.msfscc.fileinformation.FileStandardInformation;
 import com.hierynomus.mssmb2.SMB2CreateDisposition;
 import com.hierynomus.mssmb2.SMB2ShareAccess;
-import com.hierynomus.smbj.ProgressListener;
 import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
+import com.hierynomus.smbj.common.SmbPath;
 import com.hierynomus.smbj.connection.Connection;
 import com.hierynomus.smbj.session.Session;
 import com.hierynomus.smbj.share.DiskShare;
@@ -34,18 +31,18 @@ public class NetworkDriveLocationAccess {
     private static String domain = "LL";
     private static String userName = "saukedia1";
     private static String password = "Pass@may2019";
-    private static String hostname = "WKWIN2407161";
-    private static String sharename = "Users";
-    private static String remoteFilePath = "saukedia1\\Desktop\\POCFolder";
+    private static String remoteFilePath = "\\\\WKWIN2407161\\Users\\saukedia1\\Desktop\\POCFolder";
 
     public static void main(String[] args) throws Exception {
-//        sharedFolderAccessUsingJCIFSLibrary();
+        sharedFolderAccessUsingJCIFSLibrary();
         sharedFolderAccessUsingSMBJLibrary();
     }
 
     private static void sharedFolderAccessUsingJCIFSLibrary() throws IOException {
         NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(domain, userName, password);
-        SmbFile dir = new SmbFile("smb:".concat(remoteFilePath), auth);
+        String sharedSMBPath = "smb:".concat(remoteFilePath);
+        System.out.println(sharedSMBPath);
+        SmbFile dir = new SmbFile(sharedSMBPath, auth);
         for (SmbFile f : dir.listFiles()) {
             try (SmbFileInputStream in = new SmbFileInputStream(f)) {
                 byte[] bytes = new byte[(int) f.length()];
@@ -57,12 +54,16 @@ public class NetworkDriveLocationAccess {
 
     private static void sharedFolderAccessUsingSMBJLibrary() throws Exception {
         List<File> files = new ArrayList<>();
-        try (SMBClient client = new SMBClient(); Connection connection = client.connect(hostname)) {
+        SmbPath smbPath = SmbPath.parse(remoteFilePath);
+        System.out.println(smbPath.getHostname());
+        System.out.println(smbPath.getShareName());
+        System.out.println(smbPath.getPath());
+        try (SMBClient client = new SMBClient(); Connection connection = client.connect(smbPath.getHostname())) {
             AuthenticationContext ac = new AuthenticationContext(userName, password.toCharArray(), domain);
             Session session = connection.authenticate(ac);
 
-            try (DiskShare share = (DiskShare) session.connectShare("Users")) {
-                getFile(remoteFilePath, share, files);
+            try (DiskShare share = (DiskShare) session.connectShare(smbPath.getShareName())) {
+                getFile(smbPath.getPath(), share, files);
             }
         }
     }
@@ -82,21 +83,15 @@ public class NetworkDriveLocationAccess {
                 && !f.getFileName()
                     .equals("..")) {
                 Set<SMB2ShareAccess> s = new HashSet<>();
-                s.add(SMB2ShareAccess.ALL.iterator().next());
-                File file = share.openFile(currentFilePath, EnumSet.of(AccessMask.READ_CONTROL), null, s, SMB2CreateDisposition.FILE_OPEN, null);
-                ProgressListener progressListener = new ProgressListener() {
-                    
-                    @Override
-                    public void onProgressChanged(long numBytes, long totalBytes) {
-                       System.out.println("NetworkDriveLocationAccess.getFile(...).new ProgressListener() {...}.onProgressChanged()");
-                    }
-                };
-                try (InputStream in = file.getInputStream(progressListener)) {
-                    int size = (int)fileStandardInformation.getEndOfFile();
+                s.add(SMB2ShareAccess.ALL.iterator()
+                    .next());
+                try (File file = share.openFile(currentFilePath, EnumSet.of(AccessMask.GENERIC_READ), null, s, SMB2CreateDisposition.FILE_OPEN, null)) {
+                    System.out.println(f.getFileName());
+                    int size = (int) fileStandardInformation.getEndOfFile();
                     System.out.println(size);
-                    byte[] bytes = new byte[size];
-                    in.read(bytes);
-                    System.out.println(new String(bytes));
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    file.read(baos);
+                    System.out.println(baos.toString());
                 }
             }
         }
